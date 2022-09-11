b44e85d4366e4abdc72cf972cc1105557f6e0730
lizhirui <exiis@126.com>
2022-09-12 00:49:52 +0800

    tcm difftest ok

--- rtl/tcm.sv
+++ rtl/tcm.sv
@@ -2,8 +2,8 @@
 `include "common.svh"
 
 module tcm #(
-        parameter IMAGE_PATH = "D:\\program\\project\\MyRISC-VCore\\image\\coremark_10\\",
-        parameter IMAGE_INIT = 1
+        parameter IMAGE_PATH = "",
+        parameter IMAGE_INIT = 0
     )(
         input logic clk,
         input logic rst,
@@ -26,12 +26,17 @@ module tcm #(
     localparam BANK_ADDR_WIDTH = $clog2(BANK_NUM);
     localparam TCM_ADDR_WIDTH = $clog2(`TCM_SIZE);
 
-    logic[7:0] fetch_mem[0:BANK_NUM - 1][0:`TCM_SIZE / BANK_NUM - 1];    
-    logic[7:0] stbuf_mem[0:BANK_NUM - 1][0:`TCM_SIZE / BANK_NUM - 1];    
+    logic[7:0] fetch_mem[0:BANK_NUM - 1][0:`TCM_SIZE / BANK_NUM - 1];
+    logic[7:0] stbuf_mem[0:BANK_NUM - 1][0:`TCM_SIZE / BANK_NUM - 1];
+
+    logic[`ADDR_WIDTH - 1:0] bus_tcm_fetch_addr_r;
+    logic[`ADDR_WIDTH - 1:0] bus_tcm_stbuf_read_addr_r;
 
     logic[`ADDR_WIDTH - 1:0] fetch_full_addr_set[0:BANK_NUM - 1];
+    logic[`ADDR_WIDTH - 1:0] fetch_full_addr_set_r[0:BANK_NUM - 1];
     logic[`ADDR_WIDTH - BANK_ADDR_WIDTH - 1:0] fetch_bank_addr_set[0:BANK_NUM - 1];
     logic[BANK_NUM - 1:0] fetch_bank_id_cmp[0:BANK_NUM - 1];
+    logic[BANK_NUM - 1:0] fetch_bank_id_cmp_r[0:BANK_NUM - 1];
     logic[`ADDR_WIDTH - BANK_ADDR_WIDTH - 1:0] fetch_bank_addr[0:BANK_NUM - 1];
     logic[BANK_ADDR_WIDTH - 1:0] fetch_bank_set_index[0:BANK_NUM - 1];
     logic[`BUS_DATA_WIDTH - 1:0] fetch_bank_data_reg[0:BANK_NUM - 1];
@@ -39,8 +44,10 @@ module tcm #(
     logic[`BUS_DATA_WIDTH - 1:0] fetch_bank_data_recombine[0:BANK_NUM - 1];
 
     logic[`ADDR_WIDTH - 1:0] stbuf_read_full_addr_set[0:BANK_NUM - 1];
+    logic[`ADDR_WIDTH - 1:0] stbuf_read_full_addr_set_r[0:BANK_NUM - 1];
     logic[`ADDR_WIDTH - BANK_ADDR_WIDTH - 1:0] stbuf_read_bank_addr_set[0:BANK_NUM - 1];
     logic[BANK_NUM - 1:0] stbuf_read_bank_id_cmp[0:BANK_NUM - 1];
+    logic[BANK_NUM - 1:0] stbuf_read_bank_id_cmp_r[0:BANK_NUM - 1];
     logic[`ADDR_WIDTH - BANK_ADDR_WIDTH - 1:0] stbuf_read_bank_addr[0:BANK_NUM - 1];
     logic[BANK_ADDR_WIDTH - 1:0] stbuf_read_bank_set_index[0:BANK_NUM - 1];
     logic[`BUS_DATA_WIDTH - 1:0] stbuf_read_bank_data_reg[0:BANK_NUM - 1];
@@ -62,55 +69,45 @@ module tcm #(
     generate
         if(IMAGE_INIT) begin
             initial begin
-                $readmemh({IMAGE_PATH, "0.bank"}, fetch_mem[0]);
-                $readmemh({IMAGE_PATH, "1.bank"}, fetch_mem[1]);
-                $readmemh({IMAGE_PATH, "2.bank"}, fetch_mem[2]);
-                $readmemh({IMAGE_PATH, "3.bank"}, fetch_mem[3]);
-                $readmemh({IMAGE_PATH, "4.bank"}, fetch_mem[4]);
-                $readmemh({IMAGE_PATH, "5.bank"}, fetch_mem[5]);
-                $readmemh({IMAGE_PATH, "6.bank"}, fetch_mem[6]);
-                $readmemh({IMAGE_PATH, "7.bank"}, fetch_mem[7]);
-                $readmemh({IMAGE_PATH, "8.bank"}, fetch_mem[8]);
-                $readmemh({IMAGE_PATH, "9.bank"}, fetch_mem[9]);
-                $readmemh({IMAGE_PATH, "10.bank"}, fetch_mem[10]);
-                $readmemh({IMAGE_PATH, "11.bank"}, fetch_mem[11]);
-                $readmemh({IMAGE_PATH, "12.bank"}, fetch_mem[12]);
-                $readmemh({IMAGE_PATH, "13.bank"}, fetch_mem[13]);
-                $readmemh({IMAGE_PATH, "14.bank"}, fetch_mem[14]);
-                $readmemh({IMAGE_PATH, "15.bank"}, fetch_mem[15]);
-                $readmemh({IMAGE_PATH, "0.bank"}, stbuf_mem[0]);
-                $readmemh({IMAGE_PATH, "1.bank"}, stbuf_mem[1]);
-                $readmemh({IMAGE_PATH, "2.bank"}, stbuf_mem[2]);
-                $readmemh({IMAGE_PATH, "3.bank"}, stbuf_mem[3]);
-                $readmemh({IMAGE_PATH, "4.bank"}, stbuf_mem[4]);
-                $readmemh({IMAGE_PATH, "5.bank"}, stbuf_mem[5]);
-                $readmemh({IMAGE_PATH, "6.bank"}, stbuf_mem[6]);
-                $readmemh({IMAGE_PATH, "7.bank"}, stbuf_mem[7]);
-                $readmemh({IMAGE_PATH, "8.bank"}, stbuf_mem[8]);
-                $readmemh({IMAGE_PATH, "9.bank"}, stbuf_mem[9]);
-                $readmemh({IMAGE_PATH, "10.bank"}, stbuf_mem[10]);
-                $readmemh({IMAGE_PATH, "11.bank"}, stbuf_mem[11]);
-                $readmemh({IMAGE_PATH, "12.bank"}, stbuf_mem[12]);
-                $readmemh({IMAGE_PATH, "13.bank"}, stbuf_mem[13]);
-                $readmemh({IMAGE_PATH, "14.bank"}, stbuf_mem[14]);
-                $readmemh({IMAGE_PATH, "15.bank"}, stbuf_mem[15]);
+                $readmemh(IMAGE_PATH, fetch_mem);
+                $readmemh(IMAGE_PATH, stbuf_mem);
             end
         end
     endgenerate
 
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            bus_tcm_fetch_addr_r <= 'b0;
+        end
+        else if(bus_tcm_fetch_rd) begin
+            bus_tcm_fetch_addr_r <= bus_tcm_fetch_addr;
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            bus_tcm_stbuf_read_addr_r <= 'b0;
+        end
+        else if(bus_tcm_stbuf_rd) begin
+            bus_tcm_stbuf_read_addr_r <= bus_tcm_stbuf_read_addr;
+        end
+    end
+
     generate
         for(i = 0;i < BANK_NUM;i = i + 1) begin
-            assign fetch_full_addr_set[i] = bus_tcm_fetch_addr + i;
+            assign fetch_full_addr_set[i] = bus_tcm_fetch_addr + unsigned'(i);
+            assign fetch_full_addr_set_r[i] = bus_tcm_fetch_addr_r + unsigned'(i);
             assign fetch_bank_addr_set[i] = fetch_full_addr_set[i][`ADDR_WIDTH - 1:BANK_ADDR_WIDTH];
 
             for(j = 0;j < BANK_NUM;j = j + 1) begin
                 assign fetch_bank_id_cmp[i][j] = fetch_full_addr_set[j][BANK_ADDR_WIDTH - 1:0] == unsigned'(i);
+                assign fetch_bank_id_cmp_r[i][j] = fetch_full_addr_set_r[j][BANK_ADDR_WIDTH - 1:0] == unsigned'(i);
             end
 
             parallel_finder #(
                 .WIDTH(BANK_NUM)
             )parallel_finder_fetch_bank_set_index_inst(
-                .data_in(fetch_bank_id_cmp[i]),
+                .data_in(fetch_bank_id_cmp_r[i]),
                 .index(fetch_bank_set_index[i])
             );
 
@@ -124,7 +121,12 @@ module tcm #(
             );
 
             always_ff @(posedge clk) begin
-                fetch_bank_data_reg[i] <= fetch_mem[i][fetch_bank_addr[i]];
+                if(stbuf_bank_we[i] && (stbuf_write_bank_addr[i] == fetch_bank_addr[i])) begin
+                    fetch_bank_data_reg[i] <= stbuf_bank_write_data[i];
+                end
+                else begin
+                    fetch_bank_data_reg[i] <= fetch_mem[i][fetch_bank_addr[i]];
+                end
             end
 
             assign fetch_bank_data_reg_shift[i] = ($clog2(`BUS_DATA_WIDTH)'(fetch_bank_set_index[i])) << 3;
@@ -136,17 +138,19 @@ module tcm #(
 
     generate
         for(i = 0;i < BANK_NUM;i = i + 1) begin
-            assign stbuf_read_full_addr_set[i] = bus_tcm_stbuf_read_addr + i;
+            assign stbuf_read_full_addr_set[i] = bus_tcm_stbuf_read_addr + unsigned'(i);
+            assign stbuf_read_full_addr_set_r[i] = bus_tcm_stbuf_read_addr_r + unsigned'(i);
             assign stbuf_read_bank_addr_set[i] = stbuf_read_full_addr_set[i][`ADDR_WIDTH - 1:BANK_ADDR_WIDTH];
 
             for(j = 0;j < BANK_NUM;j = j + 1) begin
                 assign stbuf_read_bank_id_cmp[i][j] = stbuf_read_full_addr_set[j][BANK_ADDR_WIDTH - 1:0] == unsigned'(i);
+                assign stbuf_read_bank_id_cmp_r[i][j] = stbuf_read_full_addr_set_r[j][BANK_ADDR_WIDTH - 1:0] == unsigned'(i);
             end
 
             parallel_finder #(
                 .WIDTH(BANK_NUM)
             )parallel_finder_stbuf_read_bank_set_index_inst(
-                .data_in(stbuf_read_bank_id_cmp[i]),
+                .data_in(stbuf_read_bank_id_cmp_r[i]),
                 .index(stbuf_read_bank_set_index[i])
             );
 
@@ -160,7 +164,12 @@ module tcm #(
             );
 
             always_ff @(posedge clk) begin
-                stbuf_read_bank_data_reg[i] <= stbuf_mem[i][stbuf_read_bank_addr[i]];
+                if(stbuf_bank_we[i] && (stbuf_write_bank_addr[i] == stbuf_read_bank_addr[i])) begin
+                    stbuf_read_bank_data_reg[i] <= stbuf_bank_write_data[i];
+                end
+                else begin
+                    stbuf_read_bank_data_reg[i] <= stbuf_mem[i][stbuf_read_bank_addr[i]];
+                end
             end
 
             assign stbuf_read_bank_data_reg_shift[i] = ($clog2(`BUS_DATA_WIDTH)'(stbuf_read_bank_set_index[i])) << 3;
@@ -173,9 +182,9 @@ module tcm #(
     always_comb begin
         if(bus_tcm_stbuf_wr) begin
             case(bus_tcm_stbuf_write_size)
-                'b00: stbuf_set_we_flatten = 'b1;
-                'b01: stbuf_set_we_flatten = 'b11;
-                'b10: stbuf_set_we_flatten = 'b1111;
+                'd1: stbuf_set_we_flatten = 'b1;
+                'd2: stbuf_set_we_flatten = 'b11;
+                'd4: stbuf_set_we_flatten = 'b1111;
                 default: stbuf_set_we_flatten = 'b0;
             endcase
         end
